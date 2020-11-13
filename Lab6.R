35d16c32f5177a753f3e63a554e4592846e9d35d
sturookie <1403043489@qq.com>
2020-11-14 01:09:00 +0800

    Add files via upload

---
+++ Lab6.R
@@ -0,0 +1,91 @@
+
+library(ISLR)
+library(MASS)
+library(boot)
+set.seed(1)
+
+
+help("sample")
+train = sample(392,196)
+
+lm.fit <- lm(mpg~horsepower, data = Auto, subset = train)
+
+attach(Auto)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+
+lm.fit2 <- lm(mpg~poly(horsepower,2), data = Auto, subset = train) # Quadratic 
+mean((mpg-predict(lm.fit2,Auto))[-train]^2) 
+
+lm.fit3 <- lm(mpg~poly(horsepower,3), data = Auto, subset = train) # Cubic
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+set.seed(2)
+train = sample(392,196)
+lm.fit <- lm(mpg~horsepower, data = Auto, subset = train)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+
+lm.fit2 <- lm(mpg~poly(horsepower,2), data = Auto, subset = train) # Quadratic 
+mean((mpg-predict(lm.fit2,Auto))[-train]^2) 
+
+lm.fit3 <- lm(mpg~poly(horsepower,3), data = Auto, subset = train) # Cubic
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+install.packages("randomForest")
+library(randomForest)
+data1 <- read.csv(file.choose(), header = TRUE) 
+head(data1)
+colnames(data1) <-c("BuyingPrice", "Maintenance","NumDoors","NumPersons", "BootSpace","Safety", "Condition")
+head(data1)
+str(data1)
+levels(data1$Condition)
+summary(data1)
+
+set.seed(100)
+train <-sample(nrow(data1), 0.7*nrow(data1), replace =FALSE) 
+TrainSet <-data1 [train,]
+ValidSet <-data1[-train,]
+summary(TrainSet)
+summary(ValidSet)
+help(randomForest) 
+model1 <- randomForest(Condition~., data = TrainSet, importance = TRUE) 
+model1
+
+model2<- randomForest(Condition~.,data = TrainSet, ntree=500,mtry = 6,importance=TRUE)
+model2
+predTrain<-predict(model2,TrainSet,type="class")
+table(predTrain, TrainSet$Condition)
+preValid<-predict(model2,ValidSet, type="class")
+table(preValid, ValidSet$Condition)
+
+importance(model2)
+varImpPlot(model2)
+
+a =c()
+i=5
+for (i in 3:8){
+  model3<- randomForest(Condition~.,data = TrainSet, ntree=500,mtry = 6,importance=TRUE)
+  preValid<-predict(model3,ValidSet, type="class")
+  a[i-2] = mean(predValid ==ValidSet$Condition)
+}
+a
+plot(3:8,a)
+
+library(rpart)
+library(caret)
+library(e1071)
+
+model_dt <- train(Condition~.,data = TrainSet,method="rpart")
+model_dt_1 = predict(model_dt, data = TrainSet)
+table(model_dt_1,TrainSet$Condition)
+mean(model_dt_1==TrainSet$Condition)
+table(model_dt_1,TrainSet$Condition)
+mean(model_dt_1==TrainSet$Condition)
+
+model_dt_vs = predict(model_dt,newdata=ValidSet)
+table(model_dt_vs, ValidSet$Condition)
+mean(model_dt_vs==ValidSet$Condition)
+
+
+
+
+
